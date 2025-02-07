fce571f130df7b0216513732e6390dd18f71e7ec
milecotecnico005 <milecotecnico005@gmail.com>
2025-02-07 16:14:46 +0100

    Delete lang/vendor/adminlte directory

--- lang/vendor/adminlte/es/auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
-    'throttle' => 'Demasiados intentos de inicio de sesión. Por favor, intente de nuevo en :seconds segundos.',
-    'unauthorized' => 'Esta acción no está autorizada.',  // Traducción de "This action is unauthorized."
-];
